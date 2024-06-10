e1d2451a864854070567875f0e70f6fbc550b2b4
Willian Costa <willian95@outlook.com>
2024-06-10 16:58:33 -0300

    feat: implementação do metodo consulteSituacaoCliente no AvaliadorCreditoController

--- msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,15 +1,38 @@
 package com.willian.msavaliadorcredito.application;
 
+import com.willian.msavaliadorcredito.application.dto.SituacaoCliente;
+import com.willian.msavaliadorcredito.application.exceptions.ErroComunicacaoMicroserviceException;
+import com.willian.msavaliadorcredito.infra.repository.AvaliadorCreditoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/avaliar-credito")
-public class AvaliadorCreditoCOntroller {
+public class AvaliadorCreditoController {
+
+    @Autowired
+    private AvaliadorCreditoService avaliadorCreditoService;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @GetMapping(value = "/situacao-cliente", params = "cpf")
+    public ResponseEntity<?> consulteSituacaoCliente(@RequestParam("cpf") String cpf) {
+        try {
+            SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+            return ResponseEntity.ok().body(situacaoCliente);
+        } catch (ErroComunicacaoMicroserviceException e) {
+            assert HttpStatus.resolve(e.getStatus()) != null;
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
