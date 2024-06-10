b42088f4c8d3da788e92d14981c03a72417dfd85
Willian Costa <willian95@outlook.com>
2024-06-10 10:51:22 -0300

    build: adição do cloud gateway

---
+++ msCloudGateway/src/test/java/com/willian/msCloudGateway/MsCloudGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.msCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
