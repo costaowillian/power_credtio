003a0257f6ad95d812345bbdf1fa604b4b4e7e19
Willian Costa <willian95@outlook.com>
2024-06-10 13:52:57 -0300

    feat: criação do services de cartão

---
+++ mscartoes/src/main/java/com/willian/mscartoes/application/CartaoServices.java
@@ -0,0 +1,29 @@
+package com.willian.mscartoes.application;
+
+import com.willian.mscartoes.domain.Cartao;
+import com.willian.mscartoes.infra.CartaoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class CartaoServices {
+
+    @Autowired
+    private CartaoRepository repository;
+
+    @Transactional
+    public Cartao save(Cartao cartao) {
+        return repository.save(cartao);
+    }
+
+    public List<Cartao> getCartoesRendaMenorIgual(Long renda) {
+        BigDecimal redaBigDecimal = BigDecimal.valueOf(renda);
+        return repository.findByRendaLessThanEqual(redaBigDecimal);
+    }
+
+
+}
