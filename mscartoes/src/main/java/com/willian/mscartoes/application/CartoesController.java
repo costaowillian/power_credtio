94d6dffda88e0c5bafc6553ee46f281771678d6e
Willian Costa <willian95@outlook.com>
2024-06-10 13:53:40 -0300

    feat: criado controladro do microserviço de cartões

--- mscartoes/src/main/java/com/willian/mscartoes/application/CartoesController.java
+++ mscartoes/src/main/java/com/willian/mscartoes/application/CartoesController.java
@@ -1,17 +1,37 @@
 package com.willian.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.willian.mscartoes.application.dto.SalvarCartaoDTO;
+import com.willian.mscartoes.domain.Cartao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/cartoes")
 public class CartoesController {
 
+    @Autowired
+    private CartaoServices services;
+
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cartao> salvar(@RequestBody SalvarCartaoDTO request) {
+        Cartao cartao = request.toModel();
+        cartao = services.save(cartao);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(cartao.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(cartao);
     }
 
 }
