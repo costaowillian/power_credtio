04198a36a7d602e8f12eb6eb094a9fac09ee4eec
Willian Costa <willian95@outlook.com>
2024-06-10 15:34:17 -0300

    feat: implementação do situação cliente dto

--- msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/dto/SituacaoCliente.java
+++ msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/dto/SituacaoCliente.java
@@ -1,2 +1,36 @@
-package com.willian.msavaliadorcredito.application.dto;public class SituacaoCliente {
+package com.willian.msavaliadorcredito.application.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SituacaoCliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private DadosCliente cliente;
+    private List<CartaoCliente> cartoes;
+
+    public SituacaoCliente() {
+    }
+
+    public SituacaoCliente(DadosCliente cliente, List<CartaoCliente> cartoes) {
+        this.cliente = cliente;
+        this.cartoes = cartoes;
+    }
+
+    public DadosCliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(DadosCliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<CartaoCliente> getCartoes() {
+        return cartoes;
+    }
+
+    public void setCartoes(List<CartaoCliente> cartoes) {
+        this.cartoes = cartoes;
+    }
 }
