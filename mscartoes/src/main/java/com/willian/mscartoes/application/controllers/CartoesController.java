86fa2b387b0b7a7ccae2fb56c0d5614428dffc79
Willian Costa <willian95@outlook.com>
2024-06-10 14:49:05 -0300

    feat: implementação do metodo getCartoesPorCliente no cartões controller

--- mscartoes/src/main/java/com/willian/mscartoes/application/controllers/CartoesController.java
+++ mscartoes/src/main/java/com/willian/mscartoes/application/controllers/CartoesController.java
@@ -1,8 +1,11 @@
 package com.willian.mscartoes.application.controllers;
 
+import com.willian.mscartoes.application.dto.CartoesPorClienteResponse;
 import com.willian.mscartoes.application.dto.SalvarCartaoDTO;
 import com.willian.mscartoes.application.services.CartaoServices;
+import com.willian.mscartoes.application.services.ClienteCartaoService;
 import com.willian.mscartoes.domain.Cartao;
+import com.willian.mscartoes.domain.ClienteCartao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +14,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cartoes")
 public class CartoesController {
 
     @Autowired
-    private CartaoServices services;
+    private CartaoServices cartaoServices;
+
+    @Autowired
+    private ClienteCartaoService clienteCartaoService;
 
     @GetMapping
     public String status() {
@@ -27,7 +34,7 @@ public class CartoesController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cartao> salvar(@RequestBody SalvarCartaoDTO request) {
         Cartao cartao = request.toModel();
-        cartao = services.save(cartao);
+        cartao = cartaoServices.save(cartao);
 
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
@@ -38,8 +45,18 @@ public class CartoesController {
 
     @GetMapping(params = "renda", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Cartao>> getCartoesPorRenda(@RequestParam("renda") Long renda) {
-        List<Cartao> cartaoList = services.getCartoesRendaMenorIgual(renda);
+        List<Cartao> cartaoList = cartaoServices.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok().body(cartaoList);
+    }
+
+    @GetMapping(params = "cpf", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesPorCliente(@RequestParam("cpf") String cpf) {
+        List<ClienteCartao> lista = clienteCartaoService.listCartoesPorCpf(cpf);
+        List<CartoesPorClienteResponse> responseList = lista.stream().map(CartoesPorClienteResponse::fromModel).toList();
+        if(responseList.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(responseList);
     }
 
 }
