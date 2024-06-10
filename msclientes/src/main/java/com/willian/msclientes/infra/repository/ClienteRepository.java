b7d3fe2e1b86d6c1e611e015daceb2b14d183517
Willian Costa <willian95@outlook.com>
2024-06-10 09:49:03 -0300

    feat: add findByCPF on Cliente Repository

--- msclientes/src/main/java/com/willian/msclientes/infra/repository/ClienteRepository.java
+++ msclientes/src/main/java/com/willian/msclientes/infra/repository/ClienteRepository.java
@@ -2,6 +2,11 @@ package com.willian.msclientes.infra.repository;
 
 import com.willian.msclientes.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByCpf(String cpf);
 }
