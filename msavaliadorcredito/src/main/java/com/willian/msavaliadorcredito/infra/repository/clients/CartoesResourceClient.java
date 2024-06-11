1fdb97fb3706a290ee6db7e6f42e31d2393d507b
Willian Costa <willian95@outlook.com>
2024-06-11 09:56:27 -0300

    feat: criado acesso ao end point de getCartoesPorRenda no CartoesResourceClient

--- msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/infra/repository/clients/CartoesResourceClient.java
+++ msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/infra/repository/clients/CartoesResourceClient.java
@@ -1,5 +1,6 @@
 package com.willian.msavaliadorcredito.infra.repository.clients;
 
+import com.willian.msavaliadorcredito.application.dto.Cartao;
 import com.willian.msavaliadorcredito.application.dto.CartaoCliente;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -13,4 +14,7 @@ public interface CartoesResourceClient {
 
     @GetMapping(params = "cpf")
     ResponseEntity<List<CartaoCliente>> getCartoesPorCliente(@RequestParam("cpf") String cpf);
+
+    @GetMapping(params = "renda")
+    ResponseEntity<List<Cartao>> getCartoesPorRenda(@RequestParam("renda") Long renda);
 }
