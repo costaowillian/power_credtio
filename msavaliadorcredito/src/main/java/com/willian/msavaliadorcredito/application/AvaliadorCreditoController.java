8034da8995c289d3c8f632cea56e08f68618c127
Willian Costa <willian95@outlook.com>
2024-06-11 10:22:54 -0300

    feat: criado end point para fazer avaliação de cretido no avaliador de credito controller

--- msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ msavaliadorcredito/src/main/java/com/willian/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,15 +1,15 @@
 package com.willian.msavaliadorcredito.application;
 
+import com.willian.msavaliadorcredito.application.dto.DadosAvaliacao;
+import com.willian.msavaliadorcredito.application.dto.RetornoAvaliacaoCliente;
 import com.willian.msavaliadorcredito.application.dto.SituacaoCliente;
 import com.willian.msavaliadorcredito.application.exceptions.ErroComunicacaoMicroserviceException;
 import com.willian.msavaliadorcredito.infra.repository.AvaliadorCreditoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/avaliar-credito")
@@ -23,11 +23,25 @@ public class AvaliadorCreditoController {
         return "ok";
     }
 
-    @GetMapping(value = "/situacao-cliente", params = "cpf")
+    @GetMapping(value = "/situacao-cliente", params = "cpf", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> consulteSituacaoCliente(@RequestParam("cpf") String cpf) {
         try {
             SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
             return ResponseEntity.ok().body(situacaoCliente);
+        } catch (ErroComunicacaoMicroserviceException e) {
+            assert HttpStatus.resolve(e.getStatus()) != null;
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity realizarAvaliacao(@RequestBody DadosAvaliacao dados) throws ErroComunicacaoMicroserviceException {
+        avaliadorCreditoService.realizarAvaliacao(dados.getCpf(), dados.getRenda());
+        try {
+            RetornoAvaliacaoCliente retorno = avaliadorCreditoService.realizarAvaliacao(dados.getCpf(), dados.getRenda());
+            return ResponseEntity.ok().body(retorno);
         } catch (ErroComunicacaoMicroserviceException e) {
             assert HttpStatus.resolve(e.getStatus()) != null;
             return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
